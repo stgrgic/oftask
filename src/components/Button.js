943689493ad5fbe25b4f681a98aafe812b3567db
Stjepan Grgic <grgic.stjepan@gmail.com>
2021-09-05 23:57:25 +0100

    Basic Button component

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+	background-color: #18ccd8;
+	cursor: pointer;
+	border: none;
+	padding: 10px;
+	&:hover,
+	&:active {
+		background-color: #00aab6;
+	}
+`
+
+export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,33 @@
-import * as React from "react";
+import Button from '../components/Button'
+import React from 'react'
 
 // markup
 const IndexPage = () => {
-  return (
-    <>
-      <nav>
-        logo
-        <ul>
-          <li>About</li>
-          <li>Success Stories</li>
-          <li>FAQs</li>
-        </ul>
-      </nav>
-      <main>
-        <h1>Growth Capital ‍For Founders Like You</h1>
-        <p>
-          Supercharged funding from £10K to £2M for your marketing & inventory
-          purchases.
-        </p>
-        <a href="#">Get funded</a>
-        <button>How it works</button>
-        <p>Already have an account? Log in</p>
-        {/* TODO image */}
-        <h2>We’ve funded 100s of brands like yours</h2>
-        {/* TODO Logos */}
-      </main>
-    </>
-  );
-};
+	return (
+		<>
+			<nav>
+				logo
+				<ul>
+					<li>About</li>
+					<li>Success Stories</li>
+					<li>FAQs</li>
+				</ul>
+			</nav>
+			<main>
+				<h1>Growth Capital ‍For Founders Like You</h1>
+				<p>
+					Supercharged funding from £10K to £2M for your marketing & inventory
+					purchases.
+				</p>
+				<a href="/">Get funded</a>
+				<Button>How it works</Button>
+				<p>Already have an account? Log in</p>
+				{/* TODO image */}
+				<h2>We’ve funded 100s of brands like yours</h2>
+				{/* TODO Logos */}
+			</main>
+		</>
+	)
+}
 
-export default IndexPage;
+export default IndexPage
