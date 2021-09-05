88e0a1da43898a1494f59e24bf53f60bc13d0404
Stjepan Grgic <grgic.stjepan@gmail.com>
2021-09-05 23:32:35 +0100

    Basic html structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,29 @@ import * as React from "react";
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Home Page</h1>
-    </main>
+    <>
+      <nav>
+        logo
+        <ul>
+          <li>About</li>
+          <li>Success Stories</li>
+          <li>FAQs</li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Growth Capital ‍For Founders Like You</h1>
+        <p>
+          Supercharged funding from £10K to £2M for your marketing & inventory
+          purchases.
+        </p>
+        <a href="#">Get funded</a>
+        <button>How it works</button>
+        <p>Already have an account? Log in</p>
+        {/* TODO image */}
+        <h2>We’ve funded 100s of brands like yours</h2>
+        {/* TODO Logos */}
+      </main>
+    </>
   );
 };
 
